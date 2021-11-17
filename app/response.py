67af0747e7ed609c8bbde53b8691c17c63f78b4e
Sungeheon <elon4856@gmail.com>
2021-11-17 23:32:55 +0900

    feat: response.py 추가. - 예외상황 처리 class 생성.

---
+++ app/response.py
@@ -0,0 +1,23 @@
+class ApiException(Exception):
+    def __init__(self, code, message, extra=None):
+        self.extra = extra
+        self.code = code
+        self.message = message
+
+
+
+CREATE_COMPANY = "회사를 생성하였습니다."
+CREATE_TAG = "TAG를 생성하였습니다."
+
+INVALID_INPUT_COMPANY_NAME = "회사명이 입력되지 않았습니다"
+INVALID_INPUT_TAG = "태그가 선택되지 않았습니다."
+INVALID_INPUT_TAG_NAME = "태그명이 입력되지 않았습니다."
+INVALID_INPUT_COMPANY_NAME_AND_LANGUAGE = "회사명가 언어를 잘못 입력하셨습니다."
+
+ALREADY_EXISTS_COMPANY_NAME = "이미 존재하는 회사명입니다."
+
+CREATE_FAILED = "생성 실패"
+SEARCH_FAILED = "조건에 맞는 회사를 찾을 수 없습니다."
+
+FIND_NOT_COMPANY = "회사를 찾을수 없습니다."
+FIND_NOT_LANGUAGE = "언어를 찾을수 없습니다."
